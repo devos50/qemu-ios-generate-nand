76544c5a135f6c35861fd949284662334f227fe3
Martijn de Vos <mdmartijndevos@gmail.com>
2022-02-12 09:45:22 +0100

    Switched to one partition

--- generate_nand.c
+++ generate_nand.c
@@ -18,7 +18,7 @@
 #define FIL_ID 0x43303032
 #define FTL_CTX_VBLK_IND 0 // virtual block index of the FTL Context
 
-#define NUM_PARTITIONS 2
+#define NUM_PARTITIONS 1
 #define BOOT_PARTITION_FIRST_PAGE (NUM_PARTITIONS + 2)  // first two LBAs are for MBR and GUID header
 
 static uint32_t crc32_table[256];
@@ -206,7 +206,7 @@ uint32_t write_hfs_partition(char *filename, uint32_t page_offset) {
 	return partition_size / BYTES_PER_PAGE;
 }
 
-void write_mbr(int boot_partition_size, int filesystem_partition_offset, int filesystem_partition_size) {
+void write_mbr(int boot_partition_size) {
 	uint32_t bank, pbi, pib;
 
 	// write the MBR bytes (LBA 0)
@@ -217,10 +217,10 @@ void write_mbr(int boot_partition_size, int filesystem_partition_offset, int fil
 	boot_partition->startlba = BOOT_PARTITION_FIRST_PAGE;
 	boot_partition->size = boot_partition_size;
 
-	struct mbr_partition *filesystem_partition = (struct mbr_partition *)(mbr_page + MBR_ADDRESS + sizeof(struct mbr_partition));
-	filesystem_partition->sysid = 0xEE;
-	filesystem_partition->startlba = filesystem_partition_offset;
-	filesystem_partition->size = filesystem_partition_size;
+	// struct mbr_partition *filesystem_partition = (struct mbr_partition *)(mbr_page + MBR_ADDRESS + sizeof(struct mbr_partition));
+	// filesystem_partition->sysid = 0xEE;
+	// filesystem_partition->startlba = filesystem_partition_offset;
+	// filesystem_partition->size = filesystem_partition_size;
 
 	mbr_page[510] = 0x55;
 	mbr_page[511] = 0xAA;
@@ -233,10 +233,10 @@ void write_mbr(int boot_partition_size, int filesystem_partition_offset, int fil
 }
 
 void write_filesystem() {
-	int pages_for_boot_partition = write_hfs_partition("boot.part", BOOT_PARTITION_FIRST_PAGE);
+	int pages_for_boot_partition = write_hfs_partition("filesystem-readonly.img", BOOT_PARTITION_FIRST_PAGE);
 	printf("Required pages for boot partition: %d\n", pages_for_boot_partition);
-	int pages_for_filesystem_partition = write_hfs_partition("filesystem_full.part", BOOT_PARTITION_FIRST_PAGE + pages_for_boot_partition + 1);
-	printf("Required pages for filesystem partition: %d\n", pages_for_filesystem_partition);
+	//int pages_for_filesystem_partition = write_hfs_partition("filesystem_full.part", BOOT_PARTITION_FIRST_PAGE + pages_for_boot_partition + 1);
+	//printf("Required pages for filesystem partition: %d\n", pages_for_filesystem_partition);
 
 	// initialize the EFI header (LBA 1)
 	uint32_t bank, pbi, pib;
@@ -261,27 +261,27 @@ void write_filesystem() {
 	printf("Writing GUID boot partition entry to page %d, bank %d\n", pbi * PAGES_PER_BLOCK + pib, bank);
 	write_page(gpt_entry_boot_partition_page, (uint8_t *)spare, bank, pbi * PAGES_PER_BLOCK + pib);
 
-	// create the file system partition entry (LBA 3)
-	uint8_t *gpt_entry_filesystem_partition_page = malloc(BYTES_PER_PAGE);
-	gpt_ent *gpt_entry_filesystem_partition = (gpt_ent *)gpt_entry_filesystem_partition_page;
-	gpt_entry_filesystem_partition->ent_type[0] = 0x48465300;
-	gpt_entry_filesystem_partition->ent_type[1] = 0x11AA0000;
-	gpt_entry_filesystem_partition->ent_type[2] = 0x300011AA;
-	gpt_entry_filesystem_partition->ent_type[3] = 0xACEC4365;
-	gpt_entry_filesystem_partition->ent_lba_start = BOOT_PARTITION_FIRST_PAGE + pages_for_boot_partition + 1;
-	gpt_entry_filesystem_partition->ent_lba_end = gpt_entry_filesystem_partition->ent_lba_start + pages_for_filesystem_partition;
-	printf("File system partition located on page %lld - %lld\n", gpt_entry_filesystem_partition->ent_lba_start, gpt_entry_filesystem_partition->ent_lba_end);
+	// // create the file system partition entry (LBA 3)
+	// uint8_t *gpt_entry_filesystem_partition_page = malloc(BYTES_PER_PAGE);
+	// gpt_ent *gpt_entry_filesystem_partition = (gpt_ent *)gpt_entry_filesystem_partition_page;
+	// gpt_entry_filesystem_partition->ent_type[0] = 0x48465300;
+	// gpt_entry_filesystem_partition->ent_type[1] = 0x11AA0000;
+	// gpt_entry_filesystem_partition->ent_type[2] = 0x300011AA;
+	// gpt_entry_filesystem_partition->ent_type[3] = 0xACEC4365;
+	// gpt_entry_filesystem_partition->ent_lba_start = BOOT_PARTITION_FIRST_PAGE + pages_for_boot_partition + 1;
+	// gpt_entry_filesystem_partition->ent_lba_end = gpt_entry_filesystem_partition->ent_lba_start + pages_for_filesystem_partition;
+	// printf("File system partition located on page %lld - %lld\n", gpt_entry_filesystem_partition->ent_lba_start, gpt_entry_filesystem_partition->ent_lba_end);
 
-	get_physical_address((FTL_CXT_SECTION_START + 1) * PAGES_PER_SUBLOCK + 3, &bank, &pbi, &pib);
-	printf("Writing GUID filesystem partition entry to page %d, bank %d\n", pbi * PAGES_PER_BLOCK + pib, bank);
-	write_page(gpt_entry_filesystem_partition_page, (uint8_t *)spare, bank, pbi * PAGES_PER_BLOCK + pib);
+	// get_physical_address((FTL_CXT_SECTION_START + 1) * PAGES_PER_SUBLOCK + 3, &bank, &pbi, &pib);
+	// printf("Writing GUID filesystem partition entry to page %d, bank %d\n", pbi * PAGES_PER_BLOCK + pib, bank);
+	// write_page(gpt_entry_filesystem_partition_page, (uint8_t *)spare, bank, pbi * PAGES_PER_BLOCK + pib);
 
 	// finalize the GPT header
 	memcpy(gpt_header, GPT_HDR_SIG, 8);
 	gpt_header->hdr_revision = GPT_HDR_REVISION;
 	gpt_header->hdr_size = 0x5C; // 92 bytes
 	gpt_header->hdr_lba_table = 2;
-	gpt_header->hdr_entries = 2;
+	gpt_header->hdr_entries = NUM_PARTITIONS;
 	gpt_header->hdr_entsz = 0x80;
 	gpt_header->hdr_crc_table = crc32(gpt_entry_boot_partition_page, sizeof(gpt_ent));
 	gpt_header->hdr_crc_self = crc32((uint8_t *)gpt_header, 0x5C);
@@ -291,7 +291,7 @@ void write_filesystem() {
 	write_page(gpt_header_page, (uint8_t *)spare, bank, pbi * PAGES_PER_BLOCK + pib);
 
 	// finally, write the MBR
-	write_mbr(pages_for_boot_partition, BOOT_PARTITION_FIRST_PAGE + pages_for_boot_partition + 1, pages_for_filesystem_partition);
+	write_mbr(pages_for_boot_partition);
 }
 
 int main(int argc, char *argv[]) {
