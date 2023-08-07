89d4fb4234f6d44cc33f5be9ab6bb8290fb37f99
Zoe Harper <zoe@Zoes-MacBook-Pro.local>
2023-08-07 18:06:42 +0100

    cleaned up

--- resources/binaries/usr/libexec/cydia_/fstab.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/libexec/cydia_/bash
-export PATH=/usr/libexec/cydia_
-
-if grep -Ew 'noexec|ro' /etc/fstab >/dev/null; then
-    sed -i -e 's/\<ro\>/rw/;s/,noexec\>//' /etc/fstab
-    exit 1
-else
-    exit 0
-fi
